e477f4be7ba08e2baf8444fe73315dc7b74ad9c5
chanhihi <chanhihi55@gmail.com>
2023-04-08 23:05:52 +0900

    feat(utils): print function

--- philo/utils/util.c
+++ philo/utils/util.c
@@ -6,17 +6,53 @@
 /*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 21:52:59 by chanheki          #+#    #+#             */
-/*   Updated: 2023/04/06 04:46:43 by chanheki         ###   ########.fr       */
+/*   Updated: 2023/04/08 22:56:52 by chanheki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
-long long	get_timestamp(t_philo_info *philo_info)
+/*
+ * Description: 철학자들의 죽음을 출력한다.
+ * Param.   #1: 철학자들의 정보를 담고 있는 구조체
+ * Return     : 없음
+ */
+void	print_death(t_philosophers *philosophers)
 {
-	return (get_time() - philo_info->borntocode);
+	if (philosophers->philo_info->end_flag)
+		return ;
+	pthread_mutex_lock(&philosophers->philo_info->mutex_print);
+	printf("%lld %d died\n",
+		get_time() - philosophers->philo_info->borntocode,
+		philosophers->number);
+	pthread_mutex_unlock(&philosophers->philo_info->mutex_print);
+	philosophers->philo_info->end_flag = true;
+	philosophers->alive = false;
 }
 
+/*
+ * Description: 철학자들의 행동을 출력한다.
+ * Param.   #1: 철학자들의 정보를 담고 있는 구조체
+ *          #2: 출력할 문자열
+ * Return     : 없음
+ */
+void	print_in_mutex(t_philosophers *philosophers, char *str)
+{
+	if (philosophers->philo_info->end_flag)
+		return ;
+	pthread_mutex_lock(&philosophers->philo_info->mutex_print);
+	printf("%lld %d %s\n",
+		get_time() - philosophers->philo_info->borntocode,
+		philosophers->number, str);
+	pthread_mutex_unlock(&philosophers->philo_info->mutex_print);
+}
+
+/*
+ * Description: 현재 시각을 밀리초 단위로 변환 한다.
+ * Param.     : 없음
+ * Return     : 밀리초 단위의 시각
+ *              = (초 단위 * 1000) + (마이크로초 단위 / 1000)
+ */
 long long	get_time(void)
 {
 	struct timeval	time;
@@ -25,15 +61,29 @@ long long	get_time(void)
 	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
+/*
+ * Description: 주어진 시간 만큼 usleep을 한다.
+ * Param.   #1: usleep을 하는 데 걸리는 시간
+ * Return     : 없음
+ */
 void	my_usleep(long long duration)
 {
 	long long	now;
 
 	now = get_time();
 	while (get_time() < duration + now)
-		usleep(1);
+		usleep(100);
 }
 
+/*
+ * Description: 입력 받고 예외처리 플래그를 세운다.
+ *              1. long long 타입으로 입력을 받는다.
+ *              2. - 입력이 들어오면 validator을 &연산으로 0으로 초기화.
+ *              3. 0 입력이 들어오지 않을 때만 validator를 ll로 
+ * Param.   #1: validator
+ *          #2: 입력 문자열
+ * Return     : str to long long
+ */
 long long	ft_atoll(int *validator, const char *str)
 {
 	long long	ll;
