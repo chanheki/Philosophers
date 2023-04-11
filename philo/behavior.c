eaf0b7acfd489830986b6e866a8ae512ed3d2d80
chanhihi <chanhihi55@gmail.com>
2023-04-12 04:47:43 +0900

    bug(mutex): trouble shooting data race

--- philo/behavior.c
+++ philo/behavior.c
@@ -6,55 +6,11 @@
 /*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 21:17:32 by chanheki          #+#    #+#             */
-/*   Updated: 2023/04/08 22:37:19 by chanheki         ###   ########.fr       */
+/*   Updated: 2023/04/12 04:42:37 by chanheki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-/*
- * Description: 철학자들이 먹는다.
- *              0. 해당 철학자가 살아있는지 확인한다.
- *              1. 식사하는 동안 critical_section을 잠금한다.
- *              2. 식사 메시지를 출력한다.
- *              3. 식사를 한다 = 식사 시간 만큼 sleep한다.
- *              4. 마지막으로 식사를 한 시간을 갱신한다.
- *              5. 식사 횟수를 1 증가시킨다.
- *              6. critical_section을 잠금 해제한다.
- *              7. 왼쪽 포크를 놓는다.
- *              8. 오른쪽 포크를 놓는다.
- * Param.   #1: t_philosophers 구조체의 포인터
- * Return     : 없음
- */
-void	take_eat(t_philosophers *philosophers)
-{
-	if (!philosophers->alive)
-		return ;
-	pthread_mutex_lock(&philosophers->critical_section);
-	print_eating(philosophers);
-	philosophers->last_time_eaten = get_time();
-	philosophers->number_of_times_eaten++;
-	my_usleep(philosophers->philo_info->time_to_eat);
-	pthread_mutex_unlock(&philosophers->critical_section);
-	pthread_mutex_unlock(philosophers->left_fork);
-	pthread_mutex_unlock(philosophers->right_fork);
-}
-
-/*
- * Description: 철학자들이 잠을 잔다. 잠을 잘 땐 아무것도 하면 안된다.
- *              0. 해당 철학자가 살아있는지 확인한다.
- *              1. 잠을 잔다는 메시지를 출력한다.
- *              2. 잠을 잔다 = 잠자는 시간 만큼 m초 만큼 sleep한다.
- * Param.   #1: t_philosophers 구조체의 포인터
- * Return     : 없음
- */
-void	take_sleep(t_philosophers *philosophers)
-{
-	if (!philosophers->alive)
-		return ;
-	print_sleeping(philosophers);
-	my_usleep(philosophers->philo_info->time_to_sleep);
-}
 
 /*
  * Description: 철학자들이 생각하고나서, 철학자들이 포크를 집는다.
@@ -65,22 +21,84 @@ void	take_sleep(t_philosophers *philosophers)
  *              5. 철학자가 오른쪽 포크를 집는다.
  *              6. 포크를 집었다는 메시지를 출력한다.
  * Param.   #1: t_philosophers 구조체의 포인터
- * Return     : 없음
+ * Return     : true = 죽음
+ *              false = 살아있음
  */
-void	thinking_and_take_fork(t_philosophers *philosophers)
+bool	thinking_and_take_fork(t_philosophers *philosophers)
 {
-	if (!philosophers->alive)
-		return ;
-	print_thinking(philosophers);
+	if (print_thinking(philosophers))
+		return (true);
 	pthread_mutex_lock(philosophers->left_fork);
-	print_fork(philosophers);
-	if (philosophers->philo_info->end_flag
-		|| philosophers->philo_info->number_of_philosophers == 1)
+	if (print_fork(philosophers))
 	{
 		pthread_mutex_unlock(philosophers->left_fork);
-		philosophers->alive = false;
-		return ;
+		return (true);
+	}
+	if (philosophers->philo_info->number_of_philosophers == 1)
+	{
+		pthread_mutex_unlock(philosophers->left_fork);
+		return (true);
 	}
 	pthread_mutex_lock(philosophers->right_fork);
-	print_fork(philosophers);
+	if (print_fork(philosophers))
+	{
+		pthread_mutex_unlock(philosophers->right_fork);
+		return (true);
+	}
+	return (false);
+}
+
+/*
+ * Description: 철학자들이 먹는다.
+ *              1. 식사하는 동안 critical_section을 잠금한다.
+ *              1-2. 마지막으로 식사를 시작한 시간을 갱신한다.
+ *              1-3. critical_section을 잠금 해제한다.
+ *              2. 식사 메시지를 출력한다.
+ *              3. 식사를 한다 = 식사 시간 만큼 sleep한다.
+ *              4. 식사하는 동안 critical_section을 잠금한다.
+ *              4-1. 식사 횟수를 1 증가시킨다.
+ *              4-2. critical_section을 잠금 해제한다.
+ *              5. 왼쪽 포크를 놓는다.
+ *              6. 오른쪽 포크를 놓는다.
+ * Param.   #1: t_philosophers 구조체의 포인터
+ * Return     : true = 죽음
+ *              false = 살아있음
+ */
+bool	take_eat(t_philosophers *philosophers)
+{
+	long long	now;
+
+	now = get_time();
+	pthread_mutex_lock(&philosophers->critical_section);
+	philosophers->last_time_eaten = now;
+	pthread_mutex_unlock(&philosophers->critical_section);
+	if (print_eating(philosophers))
+	{
+		pthread_mutex_unlock(philosophers->left_fork);
+		pthread_mutex_unlock(philosophers->right_fork);
+		return (true);
+	}
+	my_usleep(philosophers->philo_info->time_to_eat);
+	pthread_mutex_lock(&philosophers->critical_section);
+	philosophers->number_of_times_eaten++;
+	pthread_mutex_unlock(&philosophers->critical_section);
+	pthread_mutex_unlock(philosophers->left_fork);
+	pthread_mutex_unlock(philosophers->right_fork);
+	return (false);
+}
+
+/*
+ * Description: 철학자들이 잠을 잔다. 잠을 잘 땐 아무것도 하면 안된다.
+ *              1. 잠을 잔다는 메시지를 출력한다.
+ *              2. 잠을 잔다 = 잠자는 시간 만큼 m초 만큼 sleep한다.
+ * Param.   #1: t_philosophers 구조체의 포인터
+ * Return     : true = 죽음
+ *              false = 살아있음
+ */
+bool	take_sleep(t_philosophers *philosophers)
+{
+	if (print_sleeping(philosophers))
+		return (true);
+	my_usleep(philosophers->philo_info->time_to_sleep);
+	return (false);
 }
--- philo/utils/print.c
+++ philo/utils/print.c
@@ -1,34 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/11 21:57:18 by chanheki          #+#    #+#             */
+/*   Updated: 2023/04/12 01:42:07 by chanheki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../philo.h"
 
 /*
  * Description: 몇번 철학자의 죽음을 출력한다.
  * Param.   #1: 철학자들의 정보를 담고 있는 구조체
- * Return     : 없음
+ * Return     : true = 죽음
  */
 void	print_death(t_philosophers *philosophers)
 {
 	long long	time;
 
 	pthread_mutex_lock(&philosophers->philo_info->during_routine);
-	if (philosophers->philo_info->end_flag)
-	{
-		pthread_mutex_unlock(&philosophers->philo_info->during_routine);
-		return ;
-	}
 	time = get_time() - philosophers->philo_info->borntocode;
 	printf("%lld %d died\n", time, philosophers->number);
 	philosophers->philo_info->end_flag = true;
-	philosophers->alive = false;
 	pthread_mutex_unlock(&philosophers->philo_info->during_routine);
 }
 
 /*
  * Description: 몇번 철학자가 포크를 집었다고 출력한다.
  * Param.   #1: 철학자들의 정보를 담고 있는 구조체
- * Return     : 없음
+ * Return     : true = 죽음
+ *              false = 살아있음
  */
-void	print_fork(t_philosophers *philosophers)
+bool	print_fork(t_philosophers *philosophers)
 {
 	long long	time;
 
@@ -36,19 +42,21 @@ void	print_fork(t_philosophers *philosophers)
 	if (philosophers->philo_info->end_flag)
 	{
 		pthread_mutex_unlock(&philosophers->philo_info->during_routine);
-		return ;
+		return (true);
 	}
 	time = get_time() - philosophers->philo_info->borntocode;
 	printf("%lld %d has taken a fork\n", time, philosophers->number);
 	pthread_mutex_unlock(&philosophers->philo_info->during_routine);
+	return (false);
 }
 
 /*
  * Description: 몇번 철학자가 밥을 먹는다는 메시지를 출력한다.
  * Param.   #1: 철학자들의 정보를 담고 있는 구조체
- * Return     : 없음
+ * Return     : true = 죽음
+ *              false = 살아있음
  */
-void	print_eating(t_philosophers *philosophers)
+bool	print_eating(t_philosophers *philosophers)
 {
 	long long	time;
 
@@ -56,19 +64,21 @@ void	print_eating(t_philosophers *philosophers)
 	if (philosophers->philo_info->end_flag)
 	{
 		pthread_mutex_unlock(&philosophers->philo_info->during_routine);
-		return ;
+		return (true);
 	}
 	time = get_time() - philosophers->philo_info->borntocode;
 	printf("%lld %d is eating\n", time, philosophers->number);
 	pthread_mutex_unlock(&philosophers->philo_info->during_routine);
+	return (false);
 }
 
 /*
  * Description: 몇번 철학자가 자는 메시지를 출력한다.
  * Param.   #1: 철학자들의 정보를 담고 있는 구조체
- * Return     : 없음
+ * Return     : true = 죽음
+ *              false = 살아있음
  */
-void	print_sleeping(t_philosophers *philosophers)
+bool	print_sleeping(t_philosophers *philosophers)
 {
 	long long	time;
 
@@ -76,19 +86,21 @@ void	print_sleeping(t_philosophers *philosophers)
 	if (philosophers->philo_info->end_flag)
 	{
 		pthread_mutex_unlock(&philosophers->philo_info->during_routine);
-		return ;
+		return (true);
 	}
 	time = get_time() - philosophers->philo_info->borntocode;
 	printf("%lld %d is sleeping\n", time, philosophers->number);
 	pthread_mutex_unlock(&philosophers->philo_info->during_routine);
+	return (false);
 }
 
 /*
  * Description: 몇번 철학자가 생각한다는 메시지를 출력한다.
  * Param.   #1: 철학자들의 정보를 담고 있는 구조체
- * Return     : 없음
+ * Return     : true = 죽음
+ *              false = 살아있음
  */
-void	print_thinking(t_philosophers *philosophers)
+bool	print_thinking(t_philosophers *philosophers)
 {
 	long long	time;
 
@@ -96,10 +108,10 @@ void	print_thinking(t_philosophers *philosophers)
 	if (philosophers->philo_info->end_flag)
 	{
 		pthread_mutex_unlock(&philosophers->philo_info->during_routine);
-		return ;
+		return (true);
 	}
 	time = get_time() - philosophers->philo_info->borntocode;
 	printf("%lld %d is thinking\n", time, philosophers->number);
 	pthread_mutex_unlock(&philosophers->philo_info->during_routine);
+	return (false);
 }
-
